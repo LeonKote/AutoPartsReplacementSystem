cd648ada0d6fbc51f18a09a3e4132f5f9518cf91
Simono <flywotbl@mail.ru>
2025-01-13 19:55:31 +0500

    Add di

--- Backend/Api/Infrastructure/DependencyInjection.cs
+++ Backend/Api/Infrastructure/DependencyInjection.cs
@@ -1,6 +1,9 @@
 using Infrastructure.Services.Car;
+using Infrastructure.Services.CarRepository;
 using Infrastructure.Services.Part;
 using Infrastructure.Services.PartReplacement;
+using Infrastructure.Services.PartReplacementRepository;
+using Infrastructure.Services.PartRepository;
 using Infrastructure.Storage;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -15,5 +18,9 @@ public static class DependencyInjection
         services.AddScoped<ICarService, CarService>();
         services.AddScoped<IPartService, PartService>();
         services.AddScoped<IPartReplacementService, PartReplacementService>();
+
+        services.AddScoped<ICarRepository, CarRepository>();
+        services.AddScoped<IPartRepository, PartRepository>();
+        services.AddScoped<IPartReplacementRepository, PartReplacementRepository>();
     }
 }
